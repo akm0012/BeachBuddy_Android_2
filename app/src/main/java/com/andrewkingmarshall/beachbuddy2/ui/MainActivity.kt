35c4df9afd735fda305bfb25d6fc79bc3c473cbd
akm0012 <akm0012@auburn.edu>
2021-04-14 19:28:16 -0400

    Added badge counter for non completed items

--- app/src/main/java/com/andrewkingmarshall/beachbuddy2/ui/MainActivity.kt
+++ app/src/main/java/com/andrewkingmarshall/beachbuddy2/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andrewkingmarshall.beachbuddy2.ui
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -34,14 +35,13 @@ class MainActivity : AppCompatActivity() {
 
         bottom_navigation_view.setupWithNavController(navController)
 
-        // todo
-//        viewModel.getNumberOfNotCompletedRequestedItems().observe(this, Observer {
-//
-//            if (it == null || it == 0) {
-//                bottom_navigation_view.removeBadge(R.id.requestedItemsFragment)
-//            } else {
-//                bottom_navigation_view.getOrCreateBadge(R.id.requestedItemsFragment).number = it
-//            }
-//        })
+        viewModel.notCompletedItemCount.observe(this, {
+
+            if (it == null || it == 0) {
+                bottom_navigation_view.removeBadge(R.id.requestedItemsFragment)
+            } else {
+                bottom_navigation_view.getOrCreateBadge(R.id.requestedItemsFragment).number = it
+            }
+        })
     }
 }
--- app/src/main/java/com/andrewkingmarshall/beachbuddy2/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/andrewkingmarshall/beachbuddy2/viewmodels/MainActivityViewModel.kt
@@ -1,17 +1,25 @@
 package com.andrewkingmarshall.beachbuddy2.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.andrewkingmarshall.beachbuddy2.repository.FirebaseRepository
+import com.andrewkingmarshall.beachbuddy2.repository.RequestedItemRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val firebaseRepository: FirebaseRepository,
+    private val requestedItemRepository: RequestedItemRepository,
 ) : ViewModel() {
+
+    val notCompletedItemCount = requestedItemRepository.getRequestedItemsDomainModel()
+        .map { it.nonCompletedItems.count() }
+        .asLiveData()
 
     init {
         viewModelScope.launch {
