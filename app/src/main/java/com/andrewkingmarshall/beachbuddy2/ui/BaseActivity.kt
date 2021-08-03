678ed3f0ac2c4ad313fe357409eb5a73d7e30008
akm0012 <akm0012@auburn.edu>
2021-08-03 10:25:14 -0400

    Code cleanup

--- app/src/main/java/com/andrewkingmarshall/beachbuddy2/ui/BaseActivity.kt
+++ app/src/main/java/com/andrewkingmarshall/beachbuddy2/ui/BaseActivity.kt
@@ -8,7 +8,6 @@ import androidx.viewbinding.ViewBinding
 abstract class BaseActivity<VB : ViewBinding>(val bindingFactory: (LayoutInflater) -> VB) : AppCompatActivity() {
 
     private var _binding: ViewBinding? = null
-//    abstract val bindingInflater: (LayoutInflater) -> VB
 
     @Suppress("UNCHECKED_CAST")
     protected val binding: VB
